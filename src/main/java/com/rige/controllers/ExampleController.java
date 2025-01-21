ec82edaded5a7a133ccf3e56280eafe920e61701
Rigebs <riclarobr04@gmail.com>
2025-01-21 14:41:00 -0500

    fix: hello world endpoint for bot

--- src/main/java/com/rige/controllers/ExampleController.java
+++ src/main/java/com/rige/controllers/ExampleController.java
@@ -2,7 +2,7 @@ package com.rige.controllers;
 
 import com.rige.dto.response.ApiResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 @AllArgsConstructor
 public class ExampleController {
-    @PostMapping("/hello")
+    @GetMapping("/hello")
     public ApiResponse hello() {
         return new ApiResponse("Hello World");
     }
